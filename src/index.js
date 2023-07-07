71950adad0964b8e73c5a259c6eff2acd7c9686a
Dora Militaru <dora@kalneus.com>
2023-07-07 12:57:48 +0100

    Minor copy adjustments

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
  * @param { FetchEvent } event
  */
 async function handleRequest(event) {
-  // Log service version
-  console.log("FASTLY_SERVICE_VERSION:", env('FASTLY_SERVICE_VERSION') || 'local');
+  // Log service version.
+  console.log("FASTLY_SERVICE_VERSION:", env("FASTLY_SERVICE_VERSION") || "local");
 
-  // The createFanoutHandoff() function creates a Response instance which informs Fastly to pass the
-  // original Request through Fanout, to the declared backend.
+  // createFanoutHandoff() creates a Response instance
+  // passing the original request, through Fanout, to the declared backend.
 
-  // NOTE: As of this writing, the request handed off to Fanout is the original request as it arrived at
-  // Compute@Edge. Modifications made to the request (such as to its headers) before calling createFanoutHandoff()
-  // will not be seen by the backend.
-  return createFanoutHandoff(event.request, 'backend');
+  // NOTE: The request handed off to Fanout is the original request 
+  // as it arrived at Compute@Edge. Any modifications made to the request 
+  // before calling createFanoutHandoff() will not be seen by the backend.
+  return createFanoutHandoff(event.request, "backend");
 }
